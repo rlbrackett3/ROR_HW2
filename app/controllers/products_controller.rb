f0a866bd1cf4118d62fbeb340d9c010bbf2d8d55
Robert Lee Brackett III <robert.brackett3@gmail.com>
2014-09-18 09:18:36 -0400

    Update products_controller.rb

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,7 +2,7 @@ class ProductsController < ApplicationController
 
 	layout 'productslayout'
 
-	def products
+	def products # In restful routing this would typically be called 'index' as it is an index of products
 		@id = params[:id] if params[:id]
 	end
 end
